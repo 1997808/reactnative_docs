1d4f5ad45f58bf5351bcf616fc4dd049fb4ab51f
1997808 <khanhhuyle22@gmail.com>
2020-03-07 14:13:52 +0700

    props basic

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import HelloWorldApp from "./src/components/HelloWorldApp.js"
 import Bananas from "./src/components/Bananas.js"
+import LotsOfGreeting from "./src/components/LotsOfGreetings.js"
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <HelloWorldApp />
+        <LotsOfGreeting />
         <Bananas />
       </React.Fragment>
     )
