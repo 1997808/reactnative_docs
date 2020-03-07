c011b44075ce52bec08228bc146df5373e6e2ea1
1997808 <khanhhuyle22@gmail.com>
2020-03-07 10:59:56 +0700

    hello world

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import HelloWorldApp from "./components/HelloWorldApp.js"
+
+export default class App extends Component {
+  render() {
+    return (
+      <HelloWorldApp />
+    )
+  }
+}
